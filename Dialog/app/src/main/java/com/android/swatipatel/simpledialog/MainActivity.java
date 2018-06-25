714de765d54960017a6318f3d98bba755b8193dc
Swati Patel <swatipatel@swatipatelmacbook.local>
2018-06-24 21:05:03 -0700

    Initial for Dialog

---
+++ Dialog/app/src/main/java/com/android/swatipatel/simpledialog/MainActivity.java
@@ -0,0 +1,22 @@
+package com.android.swatipatel.simpledialog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO
+        // Step 1:  Get Reference to the button
+
+        // Step 2 : set On click listener on the button
+
+        // Step 3 : Create Dialog in onClick method of the listener
+
+        
+    }
+}
